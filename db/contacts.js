8e8b390d25e36fee7bc25e11c24fd37efa00b99a
irysik123 <vysotskaya.iryna@gmail.com>
2023-09-10 12:41:53 -0700

    removing comments in code

--- db/contacts.js
+++ db/contacts.js
@@ -4,14 +4,6 @@ const { v4: uuidv4 } = require("uuid");
 
 let url = path.join(__dirname + "/db/contacts.json");
 
-// contacts.js
-
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
-
-// TODO: задокументувати кожну функцію
 async function listContacts() {
   return await fs
     .readFile(url)
@@ -27,7 +19,6 @@ async function getContactById(contactId) {
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   let list = await listContacts();
   let indexToDelete = list.findIndex((element) => element.id === contactId);
   if (indexToDelete >= 0) {
@@ -40,7 +31,6 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
   let list = await listContacts();
   let newContact = {
     id: uuidv4(),
@@ -56,9 +46,5 @@ async function addContact(name, email, phone) {
 async function saveContacts(list) {
   await fs.writeFile(url, JSON.stringify(list));
 }
-
-// listContacts().then((dir) => console.log(dir));
-// // addContact('liza', 'liza@mail.com', '89768878');
-// removeContact("3d015c92-3cc0-4b4f-b84b-26471b2053b0")
 
 module.exports = { addContact, removeContact, getContactById, listContacts };
